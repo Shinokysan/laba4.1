d56cce4f25c7b989e6f9ef1bf6f5908d1b2a8aae
Компьютер <@DESKTOP-0TR3ASG>
2023-10-31 20:41:31 +0700

    Добавьте файлы проекта.

---
+++ task1/task1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    int count = 0, number = -1;
+    while (number != 0) {
+        std::cin >> number;
+
+        if (number < 0 && number % 2 != 0) {
+            count += 1;
+        }
+    }
+   std::cout << count;
+
+
+}
+
